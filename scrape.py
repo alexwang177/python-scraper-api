b8172eafe65650178970584f42634d321a1740d6
Alex Wang <jianuo.wang@duke.edu>
2020-12-02 18:19:46 -0600

    able to scrape and download

--- scrape.py
+++ scrape.py
@@ -1,22 +1,17 @@
 from selenium import webdriver
-
-try:
-    import time
-except:
-    print("error time port")
+from PIL import Image
+import requests
+import hashlib
+import io
+import time
+import os
 
 
 def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
     def scroll_to_end(wd):
-        try:
-            wd.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-        except:
-            print("scrolling error")
-        try:
-            time.sleep(sleep_between_interactions)
-        except:
-            print("sleeping error")
+        wd.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)
 
     # build the google query
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
@@ -30,10 +25,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
 
     while image_count < max_links_to_fetch:
 
-        try:
-            scroll_to_end(wd)
-        except:
-            print("scroll to end error")
+        scroll_to_end(wd)
 
         # get all image thumbnail results
         thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
@@ -65,7 +57,8 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
             else:
                 print("Found:", len(image_urls),
                       "image links, looking for more...")
-                time.sleep(0.1)
+                time.sleep(0.01)
+
         load_more_button = wd.find_element_by_css_selector(".mye4qd")
         if load_more_button:
             wd.execute_script("document.querySelector('.mye4qd').click();")
@@ -76,12 +69,46 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
     return image_urls
 
 
+def persist_image(folder_path: str, url: str):
+    try:
+        image_content = requests.get(url).content
+    except Exception as e:
+        print(f"Error - Could not download {url} - {e}")
+
+    try:
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert("RGB")
+        file_path = os.path.join(folder_path, hashlib.sha1(
+            image_content).hexdigest()[:10] + ".jpg")
+
+        with open(file_path, "wb") as f:
+            image.save(f, "JPEG", quality=85)
+        print(f"Success - saved {url} - as {file_path}")
+
+    except Exception as e:
+        print(f"Error - Could not save {url} - {e}")
+
+
+def search_and_download(search_term: str, driver_path: str, target_path="./images", number_images=10):
+    target_folder = os.path.join(
+        target_path, "_".join(search_term.lower().split(" ")))
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    with webdriver.Chrome(executable_path=driver_path) as wd:
+        url_set = fetch_image_urls(
+            search_term, number_images, wd=wd, sleep_between_interactions=0.5)
+
+    for url in url_set:
+        persist_image(target_folder, url=url)
+
+
 DRIVER_PATH = "./drivers/chromedriver"
 
 wd = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 try:
-    fetch_image_urls(query="Turtles", max_links_to_fetch=100,
-                     wd=wd, sleep_between_interactions=1)
+    search_and_download("Puppies", driver_path=DRIVER_PATH)
 except:
-    print("fetching call error")
+    print("search and download error")
