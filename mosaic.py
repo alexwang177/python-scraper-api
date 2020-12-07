5a38884816dc7840384a7236048aecdedd84e897
Alex Wang <jianuo.wang@duke.edu>
2020-12-07 13:38:11 -0600

    create image grid

--- mosaic.py
+++ mosaic.py
@@ -38,3 +38,50 @@ def get_average_rgb(image):
     im = np.array(image)
     w, h, d = im.shape
     return tuple((np.average(im.reshape(w * h, d), axis=0)))
+
+
+def split_image(image, size):
+    W, H = image.size[0], image.size[1]
+    m, n = size
+    w, h = int(W / n), int(H / m)
+    imgs = []
+
+    for i in range(n):
+        for j in range(m):
+            imgs.append(image.crop(i * w, j * h, (i + 1) * w, (j + 1) * h))
+
+    return imgs
+
+
+def get_best_match_index(input_avg, avgs):
+    index = 0
+    min_index = 0
+    min_error = float("inf")
+
+    for rgb in avgs:
+        error = ((rgb[0] - input_avg[0]) * (rgb[0] - input_avg[0]) +
+                 (rgb[1] - input_avg[1]) * (rgb[1] - input_avg[1]) +
+                 (rgb[2] - input_avg[2]) * (rgb[2] - input_avg[2]))
+
+        if error < min_error:
+            min_error = error
+            min_index = index
+
+        index += 1
+
+    return min_index
+
+
+def create_image_grid(images, dimensions):
+    m, n = dimensions
+    width = max([img.size[0] for img in images])
+    height = max([img.size[1] for img in images])
+    grid_img = Image.new("RGB", (n * width, m * height))
+
+    for index in range(len(images)):
+        row = int(index / n)
+        col = index - n * row
+
+        grid_img.paste(images[index], (col * width, row * height))
+
+    return grid_img
