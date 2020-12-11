979d9bef80af96a4008f64addd8957ffdb9cc23f
Alex Wang <jianuo.wang@duke.edu>
2020-12-11 17:11:04 -0600

    no chrome options

--- python_scripts/scrape.py
+++ python_scripts/scrape.py
@@ -121,17 +121,19 @@ try:
 except:
     print("sys error")
 
+# CHROMEDRIVER_PATH = "./drivers/chromedriver"
 CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_bin
-options.add_argument(" — disable-gpu")
-options.add_argument(" — no-sandbox")
-options.add_argument(" — headless")
-wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                      chrome_options=options)
+# chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+# options = webdriver.ChromeOptions()
+# options.binary_location = chrome_bin
+# options.add_argument(" — disable-gpu")
+# options.add_argument(" — no-sandbox")
+# options.add_argument(" — headless")
+wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
 
 # Now you can start using Selenium
+
+# wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
 
 try:
     search_and_download(sys.argv[1], driver_path=CHROMEDRIVER_PATH)
