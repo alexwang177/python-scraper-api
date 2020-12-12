97628c34f3886b23120dda25492f741fd5eae01e
Alex Wang <jianuo.wang@duke.edu>
2020-12-11 18:11:04 -0600

    flag

--- python_scripts/scrape.py
+++ python_scripts/scrape.py
@@ -136,6 +136,7 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--example-flag")
 wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                       chrome_options=options)
 
