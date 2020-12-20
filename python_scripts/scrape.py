a97dbb7293d77bf09b39378c96e71f4b3ed3ae01
Alex Wang <jianuo.wang@duke.edu>
2020-12-20 11:42:28 -0600

    getting ready to deploy

--- python_scripts/scrape.py
+++ python_scripts/scrape.py
@@ -127,26 +127,26 @@ try:
 except:
     print("sys error")
 
-CHROMEDRIVER_PATH = "./drivers/chromedriver"
-# CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-# chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
-# options = webdriver.ChromeOptions()
-# options.binary_location = chrome_bin
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--headless")
-# options.add_argument('--disable-dev-shm-usage')
-# options.add_argument("--example-flag")
-# options.add_argument('--window-size=1280x1696')
-# options.add_argument('--hide-scrollbars')
-# options.add_argument('--single-process')
-# options.add_argument('--ignore-certificate-errors')
-# wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-#                       chrome_options=options)
+# CHROMEDRIVER_PATH = "./drivers/chromedriver"
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_bin
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--example-flag")
+options.add_argument('--window-size=1280x1696')
+options.add_argument('--hide-scrollbars')
+options.add_argument('--single-process')
+options.add_argument('--ignore-certificate-errors')
+wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                      chrome_options=options)
 
 # Now you can start using Selenium
 
-wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
+# wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
 
 try:
     search_and_download(sys.argv[1], driver_path=CHROMEDRIVER_PATH, wd=wd)
